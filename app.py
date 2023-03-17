25703fea5b9950e480a7bd4ef220584d9cd16095
ValentinK <krendl@technikum-wien.at>
2023-03-17 15:40:03 +0100

    curly Quotation Marks sind Böse^^

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ from wtforms.validators import DataRequired,Email,EqualTo
 
 app = Flask(__name__, template_folder='templates')
 
-app.config[‘SQLALCHEMY_DATABASE_URI’] = ‘sqlite:///data.db’
-app.config[‘SQLALCHEMY_TRACK_MODIFICATIONS’] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 SECRET_KEY = os.urandom(32)
