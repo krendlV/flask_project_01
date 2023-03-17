6b67ad84cda7b28a0933ecb68dc94fa8816a73d5
ValentinK <krendl@technikum-wien.at>
2023-03-17 15:01:07 +0100

    Use Login with SQLAlchemie das Beispiel von Hr. Ertl geht leider nicht...

--- app.py
+++ app.py
@@ -12,19 +12,37 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask import flash
 from flask import current_app, g
 from init_db import get_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
-login_manager = LoginManager()
 
 
 app = Flask(__name__, template_folder='templates')
 
-# Set the secret key to some random bytes. Keep this really secret!
-app.secret_key = b'#_3y2L_"F4Q8z\n\xec]/'
+app.config[‘SQLALCHEMY_DATABASE_URI’] = ‘sqlite:///data.db’
+app.config[‘SQLALCHEMY_TRACK_MODIFICATIONS’] = False
+db = SQLAlchemy(app)
 
-def get_db_connection():
-    conn = sqlite3.connect('data.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(50), index=True, unique=True)
+  email = db.Column(db.String(150), unique = True, index = True)
+  password_hash = db.Column(db.String(150))
+  joined_at = db.Column(db.DateTime(), default = datetime.utcnow, index = True)
+
+  def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+  def check_password(self,password):
+      return check_password_hash(self.password_hash,password)
+
+
 
 # Home sweet Home
 @app.route("/")
