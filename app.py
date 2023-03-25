4e00638b48371ffe3674a9bbb4aadaadd4785024
ValentinK <krendl@technikum-wien.at>
2023-03-25 11:26:05 +0100

    war nur ein typo...

--- app.py
+++ app.py
@@ -331,29 +331,29 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('/'))
 
-# project 2
+  # project 2
 @app.route('/project2/', methods=['POST', 'GET'])
 def project2():
-        if request.method == "POST":
-             shipnr = request.form["shipnr"]
-             found_ship = db.session.query.filter_by(Inventarnummer=shipnr).first()
-             if found_ship:
-                  session[shipnr]=found_ship
-                  seemeilen=db.session.query(found_ship.Inventarnummer, found_ship.Seemeilen).filter_by(Inventarnummer=shipnr).first()
-                  session['seemeilen']=seemeilen
-                  if seemeilen:
-                      return redirect(url_for("update.html"))
-                      
-                  else: 
+    if request.method == "POST":
+            shipnr = request.form["shipnr"]
+            found_ship = db.session.query.filter_by(Inventarnummer=shipnr).first()
+            if found_ship:
+                session[shipnr]=found_ship
+                seemeilen=db.session.query(found_ship.Inventarnummer, found_ship.Seemeilen).filter_by(Inventarnummer=shipnr).first()
+                session['seemeilen']=seemeilen
+                if seemeilen:
+                    return redirect(url_for("update.html"))
+                    
+                else: 
                     return render_template("Schiffnr.html")
-             else:
-                  return render_template("Schiffnr.html")
+            else:
+                return render_template("Schiffnr.html")
 
-        else:
-             return render_template("Schiffnr.html")
-        
+    else:
+            return render_template("Schiffnr.html")
+    
 
-@app.route('/update', method =['POST', 'GET'])
+@app.route('/update/', methods =['POST', 'GET'])
 def update():
     if request.method=="POST":
         update= Schiff(shipnr=request.form["shipnr"], Seemeilen=request.form["seemeilen"])
@@ -362,8 +362,8 @@ def update():
         return flash("Seemeilen wurden geändert")
     else:
         return render_template("update.html")
-    
-    
+  
+
 # project 3
 @app.route("/project3/", methods=['GET', 'POST'])
 def project3():
